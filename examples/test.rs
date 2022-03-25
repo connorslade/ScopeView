5aaaa635ccd84bf412ea42e722cafceba4738b96
Connor Slade <connor@connorcode.com>
2022-03-25 15:51:32 -0400

    🧼 giveup

--- examples/test.rs
+++ examples/test.rs
@@ -31,9 +31,9 @@ fn main() {
     // ren.render(Group::new().add(l1).add(l2).add(tri).add(p1));
 
     let smile = Group::new()
-        .add(Line::new(Pos::new(3.0, 3.0), Pos::new(3.0, 6.0)))
-        .add(Line::new(Pos::new(6.0, 3.0), Pos::new(6.0, 6.0)))
-        .add(Line::new(Pos::new(3.0, 7.0), Pos::new(7.0, 7.0)));
+        .add(Line::new(Pos::new(3.0, 3.0), Pos::new(3.0, 5.0)))
+        .add(Line::new(Pos::new(6.0, 3.0), Pos::new(6.0, 5.0)))
+        .add(Line::new(Pos::new(3.0, 7.0), Pos::new(6.0, 7.0)));
 
     ren.render(smile);
 }
--- src/console/mod.rs
+++ src/console/mod.rs
@@ -1,5 +1,8 @@
 use std::io::{stdout, Write};
 
+use console_engine::pixel;
+use console_engine::screen::Screen;
+
 use crate::render::Render;
 
 pub struct ConsoleRender {
@@ -14,42 +17,54 @@ impl ConsoleRender {
     }
 
     pub fn render(&self, ren: impl Render) {
-        let mut buf = vec![vec![false; self.size.0]; self.size.1];
+        // TODO: Do all this myself!
+
+        let mut engine = Screen::new(self.size.0 as u32, self.size.1 as u32);
+        engine.fill(pixel::pxl('.'));
+        // let mut buf = vec![vec![false; self.size.0]; self.size.1];
 
         for line in ren.render() {
-            let mut slope = (line.end.y - line.start.y) / (line.end.x - line.start.x);
-            let mut step = line.step;
-            if line.end.x < line.start.x {
-                slope = -slope;
-                step = -step;
-            }
-
-            let mut y = line.start.y;
-            let mut i = line.start.x;
-            buf[y as usize][i as usize] = true;
-
-            while i != line.end.x {
-                y += slope;
-                buf[y as usize][i as usize] = true;
-                i += step;
-            }
+            engine.line(
+                line.start.x as i32,
+                line.start.y as i32,
+                line.end.x as i32,
+                line.end.y as i32,
+                pixel::pxl('#'),
+            );
+            // let mut slope = (line.end.y - line.start.y) / (line.end.x - line.start.x);
+            // let mut step = line.step;
+            // if line.end.x < line.start.x {
+            //     slope = -slope;
+            //     step = -step;
+            // }
+            //
+            // let mut y = line.start.y;
+            // let mut i = line.start.x;
+            // buf[y as usize][i as usize] = true;
+            //
+            // while i != line.end.x {
+            //     y += slope;
+            //     buf[y as usize][i as usize] = true;
+            //     i += step;
+            // }
         }
 
-        let mut out = String::new();
-        for y in buf {
-            for x in y {
-                if x {
-                    out.push('#');
-                    out.push('#');
-                    continue;
-                }
-                out.push('.');
-                out.push('.');
-            }
-            out.push('\n');
-        }
+        engine.draw();
+        // let mut out = String::new();
+        // for y in buf {
+        //     for x in y {
+        //         if x {
+        //             out.push('#');
+        //             out.push('#');
+        //             continue;
+        //         }
+        //         out.push('.');
+        //         out.push('.');
+        //     }
+        //     out.push('\n');
+        // }
 
-        stdout().write(out.as_bytes()).unwrap();
-        stdout().flush().unwrap();
+        // stdout().write(out.as_bytes()).unwrap();
+        // stdout().flush().unwrap();
     }
 }
